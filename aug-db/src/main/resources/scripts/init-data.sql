fef44f1ab60836e5045181213f18aca85802e27d
amanurat <assanai17@gmail.com>
2015-05-25 14:42:47 +0700

    Update configuration for database name and initialize data for example

---
+++ aug-db/src/main/resources/scripts/init-data.sql
@@ -0,0 +1,5 @@
+insert into person(name) VALUES ("name1");
+insert into person(name) VALUES ("name2");
+insert into person(name) VALUES ("name3");
+insert into person(name) VALUES ("name4");
+insert into person(name) VALUES ("name5");
