0662a3522e377be7e2831ec078ece3efcec4d711
amanurat <assanai17@gmail.com>
2015-05-25 10:40:32 +0700

    update pom.xml to use variable properties instead of profile dev

--- aug-db/src/main/resources/scripts/create-db.sql
+++
@@ -1,8 +0,0 @@
-/*
-DROP DATABASE IF EXISTS excitedcars_db;
-CREATE DATABASE excitedcars_db;
-CREATE USER 'excitedcars'@'localhost' IDENTIFIED BY 'Password@01';
-CREATE USER 'excitedcars'@'127.0.0.1' IDENTIFIED BY 'Password@01';
-
-GRANT ALL PRIVILEGES ON excitedcars_db.* TO 'excitedcars'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON excitedcars_db.* TO 'excitedcars'@'127.0.0.1' WITH GRANT OPTION;*/
