50b19ed443a6957fc6db2897f52fa82a4a1f3df9
Net <s.neige_psn@windowslive.com>
2015-05-28 17:45:41 +0700

    Edit Package name of Service

---
+++ aug-db/src/main/java/com/aug/db/services/EducationServiceImpl.java
@@ -0,0 +1,48 @@
+package com.aug.db.services;
+
+import java.util.List;
+
+import com.aug.db.repositories.EducationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aug.db.entities.Education;
+@Service(value = "educationService")
+@Transactional
+public class EducationServiceImpl implements EducationService{
+	
+	@Autowired
+	private EducationRepository educationRepository;
+	
+	@Override
+	public Education findById(Integer id) {
+		return educationRepository.findById(id);
+	}
+
+	@Override
+	public void create(Education education) {
+		educationRepository.insert(education);
+		
+	}
+
+	@Override
+	public void update(Education education) {
+		educationRepository.update(education);
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+//		educationRepository.delete(id);
+		
+	}
+
+	@Override
+	public List<Education> findAll() {
+		List<Education> educationList = educationRepository.findAll();
+		return educationList;
+	}
+	
+
+}
---
+++ aug-db/src/main/java/com/aug/db/services/LanguagesServiceImpl.java
@@ -0,0 +1,45 @@
+package com.aug.db.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aug.db.entities.Languages;
+import com.aug.db.repositories.LanguagesRepository;
+
+@Service(value = "languagesService")
+@Transactional
+public class LanguagesServiceImpl implements LanguagesService {
+
+	@Autowired
+	private LanguagesRepository languagesRepository;
+	
+	@Rollback(value = false)
+	public void create(Languages languages) {
+		languagesRepository.insert(languages);	
+	}
+
+	public Languages findById(Integer id) {
+		return languagesRepository.findById(id);
+
+	}
+
+	@Rollback(value = false)
+	public void update(Languages languages) {
+		languagesRepository.update(languages);
+	}
+
+	@Rollback(value = false)
+	public void delete(Integer id) {
+		languagesRepository.deleteById(id);		
+	}
+
+	public List<Languages> findAll() {
+		List<Languages> languageList = languagesRepository.findAll(); 
+		return languageList;
+	}
+
+}
