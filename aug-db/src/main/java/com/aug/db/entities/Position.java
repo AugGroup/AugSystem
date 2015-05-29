3a99b3a9f94c8193fa5e38e631ff33606b11b5ae
Net <s.neige_psn@windowslive.com>
2015-05-29 09:41:58 +0700

    Edit Entity Position.java

--- aug-db/src/main/java/com/aug/db/entities/Position.java
+++ aug-db/src/main/java/com/aug/db/entities/Position.java
@@ -14,6 +14,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.ManyToAny;
 
 
