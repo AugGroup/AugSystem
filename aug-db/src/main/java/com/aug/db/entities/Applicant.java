acc835d83f07b9f2b34e6b7586fae7b72ef34c51
Rie <rie.ornchaya@gmail.com>
2015-06-03 13:51:18 +0700

    Edit Applicant.java

--- aug-db/src/main/java/com/aug/db/entities/Applicant.java
+++ aug-db/src/main/java/com/aug/db/entities/Applicant.java
@@ -3,14 +3,11 @@ package com.aug.db.entities;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
