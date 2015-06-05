ba0b2255c7b0c7ff02a62f0b76ff460a7fd6c1fc
Net <s.neige_psn@windowslive.com>
2015-06-05 14:18:32 +0700

    Add interface LoginService

---
+++ aug-db/src/main/java/com/aug/db/services/LoginService.java
@@ -0,0 +1,21 @@
+package com.aug.db.services;
+
+import java.util.List;
+
+import com.aug.db.entities.Login;
+
+public interface LoginService {
+	
+	public void create(Login login);
+	
+	public Login findById(Integer id);
+	
+	public void update(Login login);
+	
+	public void deleteById(Integer id);
+	
+	public void delete(Login login);
+	
+	public List<Login> findAll();
+
+}
