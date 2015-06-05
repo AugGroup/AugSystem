627e8cdd735fbaeabeef2c5af3614a52e335b87f
Net <s.neige_psn@windowslive.com>
2015-06-05 14:17:11 +0700

    Add interface LoginRepository

---
+++ aug-db/src/main/java/com/aug/db/repositories/LoginRepository.java
@@ -0,0 +1,9 @@
+package com.aug.db.repositories;
+
+import java.io.Serializable;
+
+import com.aug.db.entities.Login;
+
+public interface LoginRepository extends GenericRepository<Login,Serializable>{
+
+}
