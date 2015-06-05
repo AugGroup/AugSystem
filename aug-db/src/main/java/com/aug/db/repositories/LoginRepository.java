4588b2c3940b4ff064d8d27aad158d7bae766bd6
Net <s.neige_psn@windowslive.com>
2015-06-05 18:13:50 +0700

    Edit LoginRepository.java

--- aug-db/src/main/java/com/aug/db/repositories/LoginRepository.java
+++ aug-db/src/main/java/com/aug/db/repositories/LoginRepository.java
@@ -1,9 +1,13 @@
 package com.aug.db.repositories;
 
 import java.io.Serializable;
+import java.util.List;
 
+import com.aug.db.dto.LoginDTO;
 import com.aug.db.entities.Login;
 
 public interface LoginRepository extends GenericRepository<Login,Serializable>{
+	
+	public List<LoginDTO> findByUserName(String userName, String password);
 
 }
