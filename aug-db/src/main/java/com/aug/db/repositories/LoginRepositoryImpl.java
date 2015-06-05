4c11919d72307b1725bbe94dbab459c3a7b8ede2
Net <s.neige_psn@windowslive.com>
2015-06-05 18:14:51 +0700

    Edit LoginRepositoryImpl.java

--- aug-db/src/main/java/com/aug/db/repositories/LoginRepositoryImpl.java
+++ aug-db/src/main/java/com/aug/db/repositories/LoginRepositoryImpl.java
@@ -1,13 +1,27 @@
 package com.aug.db.repositories;
 
 import java.io.Serializable;
+import java.util.List;
 
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.aug.db.dto.ApplicantDTO;
+import com.aug.db.dto.LoginDTO;
 import com.aug.db.entities.Login;
 
 @Repository(value = "loginRepository")
+@Transactional
 public class LoginRepositoryImpl extends HibernateRepositoryImpl<Login,Serializable> implements LoginRepository{
+
+	@Override
+	public List<LoginDTO> findByUserName(String userName, String password) {
+		Query query = getCurrentSession().getNamedQuery("SEARCH_USERNAME");
+		query.setParameter("USERNAME", userName);
+		query.setParameter("PASSWORD", password);
+		List<LoginDTO> results = query.list();
+		return results;
+	}
 
 }
