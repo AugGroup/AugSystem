3410b8d7695e287d333abc29f37548bfb1ba8b1b
Net <s.neige_psn@windowslive.com>
2015-06-05 15:50:16 +0700

    Edit LoginRepositoryImpl.java

--- aug-db/src/main/java/com/aug/db/repositories/LoginRepositoryImpl.java
+++ aug-db/src/main/java/com/aug/db/repositories/LoginRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.aug.db.repositories;
 import java.io.Serializable;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.aug.db.entities.Login;
 
