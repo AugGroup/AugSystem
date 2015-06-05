af45245f9d5421711a31c5c33f8070a4027b12e4
Net <s.neige_psn@windowslive.com>
2015-06-05 18:16:31 +0700

    Edit LoginController.java

--- aug-rm-web/src/main/java/com/aug/controllers/LoginController.java
+++ aug-rm-web/src/main/java/com/aug/controllers/LoginController.java
@@ -6,60 +6,44 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.aug.db.entities.Applicant;
 import com.aug.db.entities.Login;
 import com.aug.db.repositories.ApplicantRepository;
 import com.aug.db.repositories.LoginRepository;
+import com.aug.db.repositories.LoginRepositoryImpl;
+import com.aug.db.services.LoginService;
 
 @Controller
 @SessionAttributes("login")
-public class LoginController implements Serializable{
-	
+public class LoginController implements Serializable {
+
 	@Autowired
-	private LoginRepository loginRepository;
-	
-	@Autowired
-	private ApplicantRepository applicantRepository;
-	
-	@RequestMapping(value="/applicant/login", method= {RequestMethod.GET})
-	public String loginSpring(){
-		System.out.println("GET GET GET GET GET GET GET GET GET");
-		List<Login> loginList = loginRepository.findAll();
-		for(Login logins : loginList){
-			System.out.println(logins.getUserName()+","+logins.getPassword());
-		}
-		List<Applicant> appList = applicantRepository.findAll();
-		for(Applicant logins : appList){
-			System.out.println(logins.getFirstNameEN());
-		}
+	private LoginService loginService;
+
+	@RequestMapping(value = "/applicant/login", method = { RequestMethod.GET })
+	public String loginSpring() {
 		return "login";
 
 	}
 
-	@RequestMapping(value = "/applicant/login", method= {RequestMethod.POST})
-	public String loginSpringPost(@RequestParam String userName,@RequestParam String password, Model model) {
-		model.addAttribute("userName", userName);
-//		model.addAttribute("password",password);
-		System.out.println("POST POST POST POST POST POST POST POST");
-//		String result="false";
-		List<Login> loginList = loginRepository.findAll();
-		for(Login logins : loginList){
-			System.out.println(logins.getUserName()+","+logins.getPassword());
-		}
-		List<Applicant> appList = applicantRepository.findAll();
-		for(Applicant logins : appList){
-			System.out.println(logins.getFirstNameEN());
-		}
-		
-//		if(password==password){
-//			result="true";
-//		}
-		return "login";
-	}
+//	@RequestMapping(value = "/applicant", method = { RequestMethod.POST })
+//	public String loginSpringPost(@ModelAttribute String userName,
+//			@ModelAttribute String password, Model model) {
+////		model.addAttribute("userName", userName);
+////		model.addAttribute("password", password);
+////		if (!userName.trim().isEmpty()) {
+////			loginService.findByUserName(userName, password);
+////			return "main_applicant";
+////		}
+//		return "main_applicant";
+//	}
 
 }
