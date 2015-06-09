4638ab727516a5411b13bc3bcfa57562197dfd9e
Net <s.neige_psn@windowslive.com>
2015-06-09 17:22:11 +0700

    Edit LoginRepositoryImpl.java

--- aug-db/src/main/java/com/aug/db/repositories/LoginRepositoryImpl.java
+++ aug-db/src/main/java/com/aug/db/repositories/LoginRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.hibernate.Query;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,15 +14,38 @@ import com.aug.db.entities.Login;
 
 @Repository(value = "loginRepository")
 @Transactional
-public class LoginRepositoryImpl extends HibernateRepositoryImpl<Login,Serializable> implements LoginRepository{
+public class LoginRepositoryImpl extends
+		HibernateRepositoryImpl<Login, Serializable> implements LoginRepository {
 
 	@Override
-	public List<LoginDTO> findByUserName(String userName, String password) {
+	public LoginDTO findByUserName(String userName) throws UsernameNotFoundException {
+		Login login = new Login();
 		Query query = getCurrentSession().getNamedQuery("SEARCH_USERNAME");
 		query.setParameter("USERNAME", userName);
-		query.setParameter("PASSWORD", password);
-		List<LoginDTO> results = query.list();
-		return results;
+		if(!"admin".equals(userName)){
+			throw new UsernameNotFoundException("login incorrect");
+		}else {
+			login.setUserName(userName);
+			login.setPassword("12345678");
+		}
+		return new LoginDTO();
+//		List<LoginDTO> results = query.list();
+//		return results;
 	}
+
+	// @Override
+	// public UserDetails loadUserByUsername(String username) throws
+	// UsernameNotFoundException {
+	// User user = new User();
+	// System.out.println("try login by user name => " + username);
+	// if (!"admin".equals(username) && !"editor".equals(username) &&
+	// !"public".equals(username)) {
+	// throw new UsernameNotFoundException("login incorrect");
+	// }else{
+	// user.setUsername(username);
+	// user.setPassword("1234");
+	// }
+	// return new UserDetaislApp(user);
+	// }
 
 }
