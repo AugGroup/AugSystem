6248b34044ce86d442a59590dfb2c35c3778c27e
Net <s.neige_psn@windowslive.com>
2015-06-09 17:20:41 +0700

    Edit LoginDTO.java

--- aug-db/src/main/java/com/aug/db/dto/LoginDTO.java
+++ aug-db/src/main/java/com/aug/db/dto/LoginDTO.java
@@ -1,10 +1,19 @@
 package com.aug.db.dto;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedNativeQueries;
 import javax.persistence.NamedNativeQuery;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.aug.db.entities.Login;
 
 @Entity
 @NamedNativeQueries({ @NamedNativeQuery(name = "SEARCH_USERNAME", query = " SELECT USERNAME, PASSWORD, "
